1d3ccb4dc2eb4a18c13fdce69e41b85ea615fea2
i-piiyush <piyushchhabra3578@gmail.com>
2024-05-09 22:28:17 +0530

    hero page

--- code/script.js
+++ code/script.js
@@ -1,25 +1,15 @@
-const navClick = document.querySelector(".navClick")
-const gola = document.querySelector(".gola")
-const gola2 = document.querySelector(".gola2")
-
-let flag = false
-
-gola.addEventListener("click",()=>{
-    
-   
-         navClick.style.top = 0
-         gola2.style.scale = 1
-        
-   
-
-})
-
-gola2.addEventListener("click",()=>{
-   
-    navClick.style.top = "-100%"
-    gola2.style.scale = 0
-   
-   
+const navClick = document.querySelector(".navClick");
+const gola = document.querySelector(".gola");
+const gola2 = document.querySelector(".gola2");
 
 
-})
+
+gola.addEventListener("click", () => {
+  navClick.style.top = 0;
+  gola2.style.scale = 1;
+});
+
+gola2.addEventListener("click", () => {
+  navClick.style.top = "-100%";
+  gola2.style.scale = 0;
+});
