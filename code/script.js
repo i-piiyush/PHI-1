b8dcad48c9d892df29395306b7fda2c65b4fc17f
i-piiyush <piyushchhabra3578@gmail.com>
2024-06-19 18:39:33 +0530

    comiited

--- code/script.js
+++ code/script.js
@@ -7,6 +7,9 @@ const navClick = document.querySelector(".navClick");
 const gola = document.querySelector(".gola");
 const gola2 = document.querySelector(".gola2");
 
+const signUp = document.querySelector(".s_up");
+const signUpPage = document.querySelector(".signup");
+const close = document.querySelector("#close");
 
 gola.addEventListener("click", () => {
   navClick.style.top = 0;
@@ -17,3 +20,10 @@ gola2.addEventListener("click", () => {
   navClick.style.top = "-100%";
   gola2.style.scale = 0;
 });
+
+signUp.addEventListener("click",()=>{
+  signUpPage.style.display="flex"
+})
+close.addEventListener("click",()=>{
+  signUpPage.style.display="none"
+})
